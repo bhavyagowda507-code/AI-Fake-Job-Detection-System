310b761bb51286a4d5bed47414404529aa659b55
Bhavya Gowda <bhavyagowda507@gmail.com>
2026-09-22 19:43:05 +0530

    Load EasyOCR only when needed

--- AI/app.py
+++ AI/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import os
 import json
-import easyocr
 import joblib
 
 from resume_ai import extract_resume_text
@@ -86,6 +85,7 @@ def get_ocr_reader():
     global ocr_reader
 
     if ocr_reader is None:
+        import easyocr
         ocr_reader = easyocr.Reader(["en"], gpu=False)
 
     return ocr_reader
